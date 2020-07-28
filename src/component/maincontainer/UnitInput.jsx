8bdcdbb67d5c32a45280a7353ae90bc99ef54821
kyoolbaba <kyoolbaba@gmail.com>
2020-07-28 19:44:55 +0530

    Added feature to restrict selection of same units in TO and FROM input fields

--- src/component/maincontainer/UnitInput.jsx
+++ src/component/maincontainer/UnitInput.jsx
@@ -5,11 +5,11 @@ import Select from 'react-select';
 import axios from 'axios'
 const baseUrl="http://localhost:8080/api/quantitymeasurement/convert/"
 const UnitInput=(props)=>{
-    const[temperature]=useState(['Farenhiet','Celcius','Kelvin'])
+    const[temperature]=useState(['Fahrenhiet','Celcius','Kelvin'])
     const[length]=useState(['Centimeter','Inch','Foot','Yard'])
     const[volume]=useState(['Liter','Milliliter','Gallon'])
     const[units]=useState([length,temperature,volume])
-    const[firstoption,updateFirstOption]=useState(1)
+    const[firstoption,updateFirstOption]=useState(0)
     const[secondoption,updateSecondOption]=useState(2)
     const[data,setData]=useState(props.unitindex)
     const[dataprev,setDataPrev]=useState(0)
@@ -19,30 +19,21 @@ const UnitInput=(props)=>{
     const[quantityOut,setQuantityOut]=useState(0);
     const unitInRef=React.useRef();
     const unitOutRef=React.useRef();
-    // const disableUnit=(e)=>{
-    //     e.target.value
-    // }
+    const inputValue=React.useRef();
 
-    // const[u,setu]=UH("");
-
-    // useEffect(()=>{
-    //     const datatook=[];
-    //     units[props.unitindex].map((e)=>{
-    //         datatook.push({value:e,label:e})
-    //     }
-    //     )
-    //     setData(datatook)
-    //     console.log(datatook)
-    // })
         
 
     useEffect(()=>{
         setUnitIn(unitInRef.current.value.toUpperCase())
         setUnitOut(unitOutRef.current.value.toUpperCase())
+        if(dataprev!==data){
+           
+        }
+
         axios.get(`${baseUrl}${unitIn}/${quantity}/${unitOut}`).then(res=>
             {
                 console.log(res)
-                setQuantityOut(res.data)      
+                setQuantityOut(res.data) 
             }
             )
 
@@ -58,25 +49,20 @@ const UnitInput=(props)=>{
     }
 
     const handleUnitInChange=(e)=>{
-        setUnitIn(e.target.value.toUpperCase())
+
+        if(e.target.value!==secondoption){
+            setUnitIn(e.target.value.toUpperCase())
+            updateFirstOption(e.target.value)
+        }
         
-        axios.get(`${baseUrl}${unitIn}/${quantity}/${unitOut}`).then(res=>
-            {
-                console.log(res)
-                setQuantityOut(res.data)      
-            }
-            )
+
     }
 
     const handleUnitOutChange=(e)=>{
-        setUnitOut(e.target.value.toUpperCase())
-        
-        axios.get(`${baseUrl}${unitIn}/${quantity}/${unitOut}`).then(res=>
-            {
-                console.log(res)
-                setQuantityOut(res.data)      
-            }
-            )
+        if(e.target.value!==firstoption){
+            setUnitOut(e.target.value.toUpperCase())
+            updateSecondOption(e.target.value)
+        }
     }
 
 
@@ -85,16 +71,16 @@ const UnitInput=(props)=>{
         <div className="UnitInput">
         <div className="UnitInput-From">
         <div className="from">From</div>
-        <input type="number" onChange={handleValueChange} value={quantity}  />
+        <input type="number" onChange={handleValueChange} ref={inputValue}  value={quantity}  />
            <div className="UnitDiv-from" >
-            <select className="UnitSelectTag" ref={unitInRef}  onChange={handleUnitInChange}   >
+            <select className="UnitSelectTag" ref={unitInRef} value={firstoption}  onChange={handleUnitInChange}   >
             {units[props.unitindex].map(
                 (unit,index)=>{
-                    let select=""
-                    if(index==={firstoption}){
-                         select="selected"
-                    }
-                  return   <option  style={{selected:{select}}}  className="UnitOptionTag"  value={unit}>{unit}</option>
+                    // let select=""
+                    // if(index==={firstoption}){
+                    //      select="selected"
+                    // }
+                  return   <option   className="UnitOptionTag"  value={unit}>{unit}</option>
                  } 
             )}
             </select>
@@ -104,18 +90,18 @@ const UnitInput=(props)=>{
         <div className="to">To</div>
         <input type="number" value={quantityOut} />
         <div className="UnitDiv-to" >
-        <select className="UnitSelectTag" ref={unitOutRef}  onChange={handleUnitOutChange} >
+        <select className="UnitSelectTag" ref={unitOutRef} value={secondoption}  onChange={handleUnitOutChange} >
         {units[props.unitindex].map(
             (unit,index)=>{
                 let select=""
                 let disable=''
-                if(index==={secondoption}){
-                     select="selected"
-                }
-                if(index==={firstoption}){
-                    disable='disabled'
-                }
-              return   <option value={unit} isOptionDisabled={true}  className="UnitOptionTag" >{unit}</option>
+                // if(index==={secondoption}){
+                //      select="selected"
+                // }
+                // if(index==={firstoption}){
+                //   return null;
+                // }
+              return   <option value={unit}  isOptionDisabled={true}  className="UnitOptionTag" >{unit}</option>
              } 
         )}
         </select>
